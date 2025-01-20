23a80c9dfcba56d7c0deaa1226b2366d0e83aee7
Osaze Oviawe <ocubed88@gmail.com>
2025-01-20 21:56:02 +0100

    - minor update

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -21,10 +21,10 @@ const links = [
     name: "Resume",
     path: "/resume",
   },
-  {
-    name: "Work",
-    path: "/work",
-  },
+  // {
+  //   name: "Work",
+  //   path: "/work",
+  // },
   {
     name: "Contact",
     path: "/contact",
